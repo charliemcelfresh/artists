20e4ed814466cba31ed9f9029eb41a48d613546f
Liam Lloyd <wlloyd@g.hmc.edu>
2020-07-27 15:20:29 -0700

    Implemented kata 17

--- cmd/root.go
+++ cmd/root.go
@@ -3,19 +3,36 @@ package cmd
 import (
 	"database/sql"
 	"errors"
+	"os"
 
+	"artists/internal/translations"
+
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "artist",
-	Short: "artist service",
+	Use:   translations.StringValues.Commands.Root.Use,
+	Short: translations.StringValues.Commands.Root.Short,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return errors.New("subcommand required")
+		return errors.New(translations.StringValues.Errors.ErrorSubcommandRequired)
 	},
 }
 
 var DB *sql.DB
+
+func init() {
+	err := godotenv.Overload()
+	if err != nil {
+		logrus.Fatal(translations.StringValues.Errors.ErrorLoadingEnv)
+	}
+	DB, err = sql.Open(translations.StringValues.SQLDrivers.Postgres, os.Getenv(translations.StringValues.EnvironmentVariables.DatabaseURL))
+	if err != nil {
+		logrus.Fatal(translations.StringValues.Errors.ErrorConnectingToDB)
+	}
+
+}
 
 // Execute launches the CLI
 func Execute() error {
--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/lib/pq v1.7.1
 	github.com/sirupsen/logrus v1.6.0
 	github.com/spf13/cobra v1.0.0
+	gopkg.in/yaml.v1 v1.0.0-20140924161607-9f9df34309c0
 )
